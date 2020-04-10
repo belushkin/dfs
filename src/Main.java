78585d9d0075e99386f0e77037cb857d3b6ef9f9
Max Belushkin <belushkin@gmail.com>
2020-04-10 14:04:20 +0300

    Added last result

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,17 @@
 import java.io.*;
 import java.util.*;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 
 public class Main {
 
-    private HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
-    private HashMap<Integer, ArrayList<Integer>> t_graph = new HashMap<>();
-    private HashMap<Integer, Boolean> visited = new HashMap<>();
+    private Map<Integer, ArrayList<Integer>> graph = new HashMap<>();
+    private Map<Integer, ArrayList<Integer>> t_graph = new HashMap<>();
+    private Map<Integer, Boolean> visited = new HashMap<>();
     private Stack<Integer> stack = new Stack<>();
 
+    private boolean yes =false;
     private int t;
-    private int k = 1;
     private TreeMap<Integer, Integer> f = new TreeMap<>(Collections.reverseOrder());
+//    private Map<Integer, Integer> f = new HashMap<>();
     private List<Integer> components = new ArrayList<>();
 
     public static void main(String[] args) {
@@ -20,38 +19,48 @@ public class Main {
         Main main = new Main();
         main.buildGraph();
         main.strongConnectedComponent();
-        System.out.println(main.getComponents());
+//        System.out.println(main.getComponents());
+        List<Integer> com = main.getComponents();
+        System.out.println(com.get(0));
+        System.out.println(com.get(1));
+        System.out.println(com.get(2));
+        System.out.println(com.get(3));
+        System.out.println(com.get(4));
 
-        System.out.println(main.getF());
+
+//        System.out.println(main.getF());
 //        System.out.println(main.getGraph());
 //        System.out.println(main.getT_graph());
     }
 
     public List<Integer> getComponents() {
+        components.sort(Collections.reverseOrder());
         return components;
     }
 
-    public HashMap<Integer, ArrayList<Integer>> getGraph() {
+    public Map<Integer, ArrayList<Integer>> getGraph() {
         return graph;
     }
 
-    public HashMap<Integer, ArrayList<Integer>> getT_graph() {
+    public Map<Integer, ArrayList<Integer>> getT_graph() {
         return t_graph;
     }
 
-    public TreeMap<Integer, Integer> getF() {
+    public Map<Integer, Integer> getF() {
         return f;
     }
 
     private void strongConnectedComponent() {
-        DFSLoop(graph);
-//        DFSLoopT();
+//        System.out.println(graph);
+        DFSLoop();
+        stack.clear();
+        DFSLoopT();
     }
 
-    private void DFSLoop(HashMap<Integer, ArrayList<Integer>> graph) {
+    private void DFSLoop() {
         graph.forEach((integer, integers) -> {
             if (!visited.containsKey(integer)) {
-                System.out.println("Loop: " + integer);
+//                System.out.println("Loop: " + integer);
                 DFSR(graph, integer);
             }
         });
@@ -68,34 +77,58 @@ public class Main {
         }));
     }
 
-    private void DFSR(HashMap<Integer, ArrayList<Integer>> graph, int start) {
-        visited.put(start, true);
-        f.put(k, start);
-        stack.push(start);
-        while (!stack.isEmpty()) {
-            int v = stack.pop();
-            graph.get(v).forEach(integer -> {
-                if (!visited.containsKey(integer)) {
-                    k += 1;
-                    visited.put(integer, true);
-                    f.put(k, integer);
-                    stack.push(integer);
-                }
-            });
-        }
-
-
+    private void DFSR(Map<Integer, ArrayList<Integer>> graph, int start) {
+//        stack.push(start);
 //        visited.put(start, true);
-//        graph.get(start).forEach(integer -> {
-//            if (!visited.containsKey(integer)) {
-//                DFSR(graph, integer);
+//        while (!stack.isEmpty()) {
+//            int V = stack.peek();
+//            yes = true;
+//            List<Integer> items = graph.get(V);
+//            for (int i = items.size()-1; i >=0; i--) {
+//                if (!visited.containsKey(items.get(i))) {
+//                    yes = false;
+//                    visited.put(items.get(i), true);
+//                    stack.push(items.get(i));
+//                }
 //            }
-//        });
-//        t++;
-//        f.put(t,start);
+//
+//            if (yes) {
+//                t += 1;
+//                int g = stack.pop();
+//                f.put(t, g);
+//            }
+//        }
+
+        visited.put(start, true);
+        graph.get(start).forEach(integer -> {
+            if (!visited.containsKey(integer)) {
+                DFSR(graph, integer);
+            }
+        });
+        t++;
+        f.put(t,start);
     }
 
-    private void DFSRT(HashMap<Integer, ArrayList<Integer>> graph, int start) {
+    private void DFSRT(Map<Integer, ArrayList<Integer>> graph, int start) {
+//        stack.push(start);
+//        visited.put(start, true);
+//        while (!stack.isEmpty()) {
+//            yes = true;
+//            List<Integer> items = graph.get(stack.peek());
+//            for (int i = items.size()-1; i >=0; i--) {
+//                if (!visited.containsKey(items.get(i))) {
+//                    yes = false;
+//                    visited.put(items.get(i), true);
+//                    stack.push(items.get(i));
+//                }
+//            }
+//
+//            if (yes) {
+//                t += 1;
+//                stack.pop();
+//            }
+//        }
+
         visited.put(start, true);
         graph.get(start).forEach(integer -> {
             if (!visited.containsKey(integer)) {
@@ -103,10 +136,12 @@ public class Main {
             }
         });
         t++;
+
     }
 
     private void buildGraph() {
-        try (Scanner scanner = new Scanner(new File("src/test_08/test_08_1.txt"))) {
+//        try (Scanner scanner = new Scanner(new File("src/test_08/test_08_2.txt"))) {
+        try (Scanner scanner = new Scanner(new File("src/test_08/input_08.txt"))) {
 
             while (scanner.hasNext()) {
                 String s = scanner.nextLine();
@@ -119,7 +154,7 @@ public class Main {
         }
     }
 
-    private void addVertex(HashMap<Integer, ArrayList<Integer>> graph, int key, int value) {
+    private void addVertex(Map<Integer, ArrayList<Integer>> graph, int key, int value) {
         ArrayList<Integer> items = new ArrayList<>();
         if (graph.containsKey(key)) {
             items = graph.get(key);
